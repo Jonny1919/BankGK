f24445606470b314bc30bbed12eeaa8cc2abed96
Jonny1919 <m-bohnensack@web.de>
2025-08-20 13:37:35 +0200

    Update upload-image.js

--- netlify/functions/upload-image.js
+++ netlify/functions/upload-image.js
@@ -1,10 +1,7 @@
 export async function handler(event, context) {
-  console.log("=== Upload Function aufgerufen ===");
-  console.log("Event Body:", event.body);
-
+  console.log("Upload Function aufgerufen");
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Function erreicht!" })
   };
 }
-
