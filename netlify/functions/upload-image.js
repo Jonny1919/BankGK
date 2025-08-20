29339898da182f827a2ba12abfe03dc009e44aee
Jonny1919 <m-bohnensack@web.de>
2025-08-20 13:30:57 +0200

    Update upload-image.js

--- netlify/functions/upload-image.js
+++ netlify/functions/upload-image.js
@@ -1,50 +1,9 @@
-const FormData = require("form-data");
-const fetch = require("node-fetch");
+export async function handler(event, context) {
+  console.log("=== Upload Function aufgerufen ===");
+  console.log("Event Body:", event.body);
 
-exports.handler = async (event) => {
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
-
-  const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
-  const uploadPreset = process.env.CLOUDINARY_UPLOAD_PRESET;
-
-  try {
-    // FormData mit dem Bild (Base64 oder Blob) vom Frontend empfangen
-    const { imageBase64 } = JSON.parse(event.body);
-
-    if (!imageBase64) {
-      return { statusCode: 400, body: "Kein Bild erhalten" };
-    }
-
-    const form = new FormData();
-    form.append("file", imageBase64);
-    form.append("upload_preset", uploadPreset);
-    form.append("folder", "Galerie");
-
-    const res = await fetch(
-      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-      {
-        method: "POST",
-        body: form,
-      }
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      return {
-        statusCode: res.status,
-        body: JSON.stringify({ error: data.error.message || "Upload-Fehler" }),
-      };
-    }
-
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ url: data.secure_url }),
-    };
-  } catch (err) {
-    return { statusCode: 500, body: JSON.stringify({ error: err.toString() }) };
-  }
-};
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Function erreicht!" })
+  };
+}
